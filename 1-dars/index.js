87453a4dcc5a41727693482686226c1d20e5be6b
abuhacker01 <abuhacker012@gmail.com>
2023-04-28 16:19:38 +0500

    works

---
+++ 1-dars/index.js
@@ -0,0 +1,8 @@
+// npx create-reat-app :
+
+/// biz bu buyruq bilan rreac papka ochib olamiz .
+
+/// va (--npx create-reat-app--)-dan kegin papkaa nimini yozib olishimiz kerak ;
+//// papkani nomlashnixam o'zini qonunlari bor :
+///// 1. papka nomi (--react--) bo'lmasligi kerak
+///// 2. papka nomini bosh xarifi katta xarif bo'lmasligi kerak
