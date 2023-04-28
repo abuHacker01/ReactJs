312ce1d6aa41f31fb9e211dabbe728bb642310ad
abuhacker01 <abuhacker012@gmail.com>
2023-04-28 23:17:28 +0500

    works

--- 1-dars/index.js
+++ 1-dars/index.js
@@ -6,3 +6,32 @@
 //// papkani nomlashnixam o'zini qonunlari bor :
 ///// 1. papka nomi (--react--) bo'lmasligi kerak
 ///// 2. papka nomini bosh xarifi katta xarif bo'lmasligi kerak
+
+// npm start :
+
+/// (--npm start--) huddi html-daggi (--live server--)-ga o'xshaydi ;
+/// uni terminalda yozganda reat papkamiz browserda ochib beradi ;
+/// uni biz ochmoxchi bo'lgan papkaga kirib kegin yoziladi ;
+
+// (--ReactJS--)-ni serveri 3000-localhost yuradi
+
+// README.md :
+
+/// proect qanday yurishi haqida malumot beradi ;
+/// uni ichidagi malumotlarni istaganimizcha o'zgartirsag bo'ladi ;
+/// biz uni o'chirib yuborsagxam umuman farqi yo'q. U faqat git hub-da umumiy malumot berish uchun ishlatiladi ;
+
+// package.json :
+
+/// mashinani tex pasportiga o'xshatsag bo'ladi ;
+
+/// yani proect xaqida malumot, terminalda qanday kod yozish :
+//// (--npm start--)-ni (--npm boshla--)-ga o'zgartirib qo'ysag bo'ladi ;
+
+/// biz bilishimiz kerak bo'ladiganlari :
+
+//// 1. dependencies :
+///// proectga qaysi kutubxonalar o'rnatilgani ;
+
+//// 2. scripts :
+///// terminalda yoziladigan codelar : qanday yurg'izish, test qilish
