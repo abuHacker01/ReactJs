ec23ed9a734bec92ebb67a0ff3e7bac7b8558538
abuhacker01 <abuhacker012@gmail.com>
2023-05-08 13:30:50 +0500

    works

---
+++ all/src/App.js
@@ -0,0 +1,12 @@
+import REact, { Component } from "react";
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>App</h1>
+                <h1>App</h1>
+            </div>
+        );
+    }
+}
--- all/src/index.js
+++ all/src/index.js
@@ -1,18 +1,9 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-let username = "Dilmurod";
-
-
-
-
-
-
-
-
-
-
+import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>Hi {  }</h1>
+    <StrictMode>
+        <App />
+    </StrictMode>
 );
